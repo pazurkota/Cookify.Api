3ef591e6798ddd20b36991be9da33e7d5711fe93
⭐PazurK0TA <piotrek.jelonek@icloud.com>
2026-02-28 12:55:28 +0100

    Changes in Program.cs

--- Cookify.Api/Program.cs
+++ Cookify.Api/Program.cs
@@ -16,11 +16,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/health", () => "app status: working");
 
 app.Run();
