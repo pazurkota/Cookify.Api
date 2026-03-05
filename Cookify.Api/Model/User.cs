bb395a12453edcc89ed8dc88e363778978a52ed1
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-03-05 21:11:32 +0100

    Created User.cs

---
+++ Cookify.Api/Model/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Cookify.Api.Model;
+
+public class User : IdentityUser
+{
+    public required string Username { get; set; }
+    public string? Bio { get; set; }
+    public string? UserAvatarUrl { get; set; }
+
+    public DateTime JoinedAt { get; set; } = DateTime.Now;
+}
