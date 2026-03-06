ee26bae8af9b2ed1cd005634f2baa58089cc4d6c
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-03-06 22:57:53 +0100

    Initialize database migration and configure DbContext for PostgreSQL

--- Cookify.Api/Program.cs
+++ Cookify.Api/Program.cs
@@ -1,10 +1,19 @@
+using Cookify.Api.Database;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
 
 var app = builder.Build();
 
