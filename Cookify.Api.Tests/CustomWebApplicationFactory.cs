6e35b6611df53ed03f9bbdbbd2bc6296dd5f3e89
Piotr Jelonek <piotrek.jelonek@icloud.com>
2026-03-09 16:59:56 +0100

    Created integration test project

---
+++ Cookify.Api.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Cookify.Api.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace Cookify.Api.Tests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:18")
+        .WithDatabase("testdb")
+        .WithUsername("test")
+        .WithPassword("test")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+        });
+        
+        builder.UseSetting("Jwt:Key", "f16459d8a179a8cf07b1a80ebf2997ea");
+        builder.UseSetting("Jwt:Issuer", "TestIssuer");
+        builder.UseSetting("Jwt:Audience", "TestAudience");
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.DisposeAsync();
+    }
+}
--- Cookify.Api/Program.cs
+++ Cookify.Api/Program.cs
@@ -91,3 +91,5 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
